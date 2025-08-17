dfb423627b372cb2c06ac11fc5d07dd3132f107b
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2025-08-17 22:44:39 +0200

    fix: rendering issue

--- src/main/java/dev/opalsopl/animania_refresh/blocks/entities/client/TroughBlockModel.java
+++ src/main/java/dev/opalsopl/animania_refresh/blocks/entities/client/TroughBlockModel.java
@@ -3,7 +3,11 @@ package dev.opalsopl.animania_refresh.blocks.entities.client;
 import dev.opalsopl.animania_refresh.AnimaniaRefresh;
 import dev.opalsopl.animania_refresh.blocks.entities.TroughBlockEntity;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.cache.object.GeoBone;
+import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.model.GeoModel;
+
+import java.util.Optional;
 
 public class TroughBlockModel extends GeoModel<TroughBlockEntity> {
     @Override
@@ -19,5 +23,17 @@ public class TroughBlockModel extends GeoModel<TroughBlockEntity> {
     @Override
     public ResourceLocation getAnimationResource(TroughBlockEntity animatable) {
         return ResourceLocation.fromNamespaceAndPath(AnimaniaRefresh.MODID, "animations/trough_fluid.animation.json");
+    }
+
+    @Override
+    public void setCustomAnimations(TroughBlockEntity animatable, long instanceId, AnimationState<TroughBlockEntity> animationState) {
+        Optional<GeoBone> fluidOptional = getBone("fluid");
+        Optional<GeoBone> foodOptional = getBone("food");
+
+        if (fluidOptional.isPresent() && foodOptional.isPresent())
+        {
+            fluidOptional.get().setHidden(animatable.isEmpty());
+            foodOptional.get().setHidden(animatable.items.getStackInSlot(0).isEmpty());
+        }
     }
 }
--- src/main/java/dev/opalsopl/animania_refresh/blocks/entities/client/TroughBlockRenderer.java
+++ src/main/java/dev/opalsopl/animania_refresh/blocks/entities/client/TroughBlockRenderer.java
@@ -1,13 +1,7 @@
 package dev.opalsopl.animania_refresh.blocks.entities.client;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import dev.opalsopl.animania_refresh.blocks.entities.TroughBlockEntity;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import software.bernie.geckolib.cache.object.GeoBone;
-import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoBlockRenderer;
 import software.bernie.geckolib.renderer.GeoRenderer;
 import software.bernie.geckolib.renderer.layer.GeoRenderLayer;
@@ -22,16 +16,6 @@ public class TroughBlockRenderer extends GeoBlockRenderer<TroughBlockEntity> {
     public static class TroughBlockRenderLayer extends GeoRenderLayer<TroughBlockEntity> {
         public TroughBlockRenderLayer(GeoRenderer entityRendererIn) {
             super(entityRendererIn);
-        }
-
-        @Override
-        public void renderForBone(PoseStack poseStack, TroughBlockEntity animatable, GeoBone bone, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, float partialTick, int packedLight, int packedOverlay) {
-            super.renderForBone(poseStack, animatable, bone, renderType, bufferSource, buffer, partialTick, packedLight, packedOverlay);
-
-            if (bone.getName().equals("fluid"))
-            {
-                bone.setHidden(animatable.tank.isEmpty());
-            }
         }
     }
 }
