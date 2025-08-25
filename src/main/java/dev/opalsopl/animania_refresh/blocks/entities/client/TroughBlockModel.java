83f6c7d9b5b8c99e5dd31f5679768979dcb73b7e
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2025-08-26 01:58:12 +0200

    refactor: use new model structure

--- src/main/java/dev/opalsopl/animania_refresh/blocks/entities/client/TroughBlockModel.java
+++ src/main/java/dev/opalsopl/animania_refresh/blocks/entities/client/TroughBlockModel.java
@@ -29,28 +29,25 @@ public class TroughBlockModel extends GeoModel<TroughBlockEntity> {
     public void setCustomAnimations(TroughBlockEntity animatable, long instanceId, AnimationState<TroughBlockEntity> animationState) {
         Optional<GeoBone> fluidOptional = getBone("fluid");
         Optional<GeoBone> foodOptional = getBone("food");
+        Optional<GeoBone> contentOptional = getBone("content");
 
-        if (fluidOptional.isPresent() && foodOptional.isPresent())
-        {
-            fluidOptional.get().setHidden(animatable.isEmpty());
-            foodOptional.get().setHidden(animatable.items.getStackInSlot(0).isEmpty());
-
-            convertStateToAnimation(animatable, fluidOptional.get());
-        }
+        foodOptional.ifPresent(bone -> bone.setHidden(animatable.items.getStackInSlot(0).isEmpty()));
+        fluidOptional.ifPresent(bone -> bone.setHidden(animatable.isEmpty()));
+        contentOptional.ifPresent(bone -> convertStateToAnimation(animatable, bone));
     }
 
-    private void convertStateToAnimation(TroughBlockEntity animatable, GeoBone fluidBone)
+    private void convertStateToAnimation(TroughBlockEntity animatable, GeoBone contentBone)
     {
         int size = animatable.getSize();
 
         switch (size)
         {
-            case 1, 100 -> fluidBone.setPosY(-4.9f); //used -4.9f instead of -5 to fix Z-fighing
-            case 200, 300 -> fluidBone.setPosY(-4);
-            case 2, 400, 500 -> fluidBone.setPosY(-3);
-            case 600, 700 -> fluidBone.setPosY(-2);
-            case 3, 800, 900 -> fluidBone.setPosY(-1);
-            default -> fluidBone.setPosY(0);
+            case 1, 100 -> contentBone.setPosY(-4.9f); //used -4.9f instead of -5 to fix Z-fighing
+            case 200, 300 -> contentBone.setPosY(-4);
+            case 2, 400, 500 -> contentBone.setPosY(-3);
+            case 600, 700 -> contentBone.setPosY(-2);
+            case 3, 800, 900 -> contentBone.setPosY(-1);
+            default -> contentBone.setPosY(0);
         }
     }
 }
