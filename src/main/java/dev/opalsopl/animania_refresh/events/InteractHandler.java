e5db98c7ef5fcc1f8bb288aafd4a48004ba86980
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2025-08-31 21:33:21 +0200

    feat: play sound and take one item on seed placing

--- src/main/java/dev/opalsopl/animania_refresh/events/InteractHandler.java
+++ src/main/java/dev/opalsopl/animania_refresh/events/InteractHandler.java
@@ -6,6 +6,9 @@ import dev.opalsopl.animania_refresh.blocks.SeedCarpetBlock;
 import dev.opalsopl.animania_refresh.types.ESeedCarpetType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
@@ -24,6 +27,7 @@ public class InteractHandler {
         BlockPos pos = event.getPos();
         Level level = event.getLevel();
         Direction direction = event.getHitVec().getDirection();
+        Player player = event.getEntity();
 
         if (stack.isEmpty() ||
             !(stack.is(Items.WHEAT_SEEDS) || stack.is(Items.BEETROOT_SEEDS) ||
@@ -53,5 +57,11 @@ public class InteractHandler {
         else return;
 
         level.setBlockAndUpdate(targetPos, AllBlocks.SEED_CARPET_BLOCK.get().defaultBlockState().setValue(SeedCarpetBlock.TYPE, ESeedCarpetType.convert(stack)));
+        level.playSound(player, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.GRASS_BREAK, SoundSource.BLOCKS, 1f ,1f);
+
+        if (!player.isCreative())
+        {
+            stack.shrink(1);
+        }
     }
 }
