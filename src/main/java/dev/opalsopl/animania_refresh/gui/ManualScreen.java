27c462143d500bad7d1bd24aa5aa49cedbd31194
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2025-09-03 22:58:08 +0200

    feat: render static text

--- src/main/java/dev/opalsopl/animania_refresh/gui/ManualScreen.java
+++ src/main/java/dev/opalsopl/animania_refresh/gui/ManualScreen.java
@@ -4,13 +4,11 @@ import dev.opalsopl.animania_refresh.helper.ResourceHelper;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
 
 public class ManualScreen extends Screen {
     public static final ResourceLocation BOOK_LOCATION = ResourceHelper.getModResourceLocation("textures/gui/book.png");
-    private MutableComponent pageMsg;
-
 
     public ManualScreen(Component title) {
         super(title);
@@ -23,6 +21,18 @@ public class ManualScreen extends Screen {
         this.renderBackground(graphics);
         int i = (this.width - 192) / 2;
         graphics.blit(BOOK_LOCATION, i, 2, 0, 0, 192, 192);
+
+        //BookViewScreen`
+        Component string = Component.literal("Lorem ipsum dolor sit amet consectetur adipiscing elit. Pretium tellus duis convallis tempus leo eu aenean. Iaculis massa nisl malesuada lacinia integer nunc posuere. Conubia nostra inceptos himenaeos orci varius natoque penatibus. Nulla molestie mattis scelerisque maximus ege");
+
+        var textLines = this.font.split(string, 114);
+
+        int maxHeight = Math.min(128 / 9, textLines.size());
+
+        for(int l = 0; l < maxHeight; ++l) {
+            FormattedCharSequence formattedcharsequence = textLines.get(l);
+            graphics.drawString(this.font, formattedcharsequence, i + 36, 32 + l * 9, 0, false);
+        }
 
     }
 }
