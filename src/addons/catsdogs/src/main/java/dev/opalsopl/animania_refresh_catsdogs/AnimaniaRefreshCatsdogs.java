42249bd95b2a6aebf2461d18046c3b7437c08d71
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2025-10-01 22:40:29 +0200

    chore: rename `AnimaniaRefreshCnd` to `AnimaniaRefreshCatsdogs` for naming consistency

--- src/addons/catsdogs/src/main/java/dev/opalsopl/animania_refresh_catsdogs/AnimaniaRefreshCatsdogs.java
+++ src/addons/catsdogs/src/main/java/dev/opalsopl/animania_refresh_catsdogs/AnimaniaRefreshCatsdogs.java
@@ -1,14 +1,14 @@
-package dev.opalsopl.animania_refresh_cnd;
+package dev.opalsopl.animania_refresh_catsdogs;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod("animania_refresh_cnd")
-public class AnimaniaRefreshCnd {
+@Mod("animania_refresh_catsdogs")
+public class AnimaniaRefreshCatsdogs {
 
-    public AnimaniaRefreshCnd() {
+    public AnimaniaRefreshCatsdogs() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
     }
